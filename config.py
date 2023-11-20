585e60b4233bf26eca2f2a443e6b7c97e3554051
gnralo <145225135+gnralo@users.noreply.github.com>
2023-11-20 19:23:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6868876488:AAHlFlaxrLVK0Zdvp_ZOJ-GlAXB6eNNXZVI")
+
+    APP_ID = int(os.environ.get("APP_ID", 24447363))
+
+    API_HASH = os.environ.get("API_HASH", "5400d05077ad972cd19fb317af63abec")
